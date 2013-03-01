7670db1e8702e4953b5599824c7dbe638b5153ca
Lara Meriguetti <lara.meriguetti@ideais.com.br>
2013-03-01 17:36:55 -0300

    resolvendo conflitos

---
+++ Riddler/src/main/java/br/com/ideais/estagio/model/Etapa.java
@@ -0,0 +1,76 @@
+package br.com.ideais.estagio.model;
+
+import java.io.Serializable;
+import br.com.ideais.estagio.model.Beneficio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+public class Etapa implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+    @SequenceGenerator( name = "sequence", sequenceName = "sequence" )
+    @GeneratedValue(generator = "sequence", strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private Integer ordem;
+	private Integer vencimento;
+	private Beneficio beneficioId;
+	
+	
+
+	private Etapa(){
+		
+	}
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	
+	public Integer getOrdem() {
+		return ordem;
+	}
+	
+	
+	public Integer getVencimento() {
+		return vencimento;
+	}
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public void setOrdem(Integer ordem) {
+		this.ordem = ordem;
+	}
+	
+	
+	public void setVencimento(Integer vencimento) {
+		this.vencimento = vencimento;
+	}
+
+
+	public Beneficio getBeneficioId() {
+		return beneficioId;
+	}
+
+}
