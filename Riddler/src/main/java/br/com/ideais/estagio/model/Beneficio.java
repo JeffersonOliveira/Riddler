e211cc00353bb8210f088e132047ef6c951420f1
Douglas <douglas@douglas.(none)>
2013-03-01 17:17:01 -0300

    commit classe beneficio

---
+++ Riddler/src/main/java/br/com/ideais/estagio/model/Beneficio.java
@@ -0,0 +1,51 @@
+package br.com.ideais.estagio.model;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Entity
+@Component
+public class Beneficio implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+		@Id
+		@SequenceGenerator( name = "sequence", sequenceName = "sequence" )
+		@GeneratedValue(generator = "sequence", strategy=GenerationType.AUTO)
+		private Long id;
+		
+		private String nome;
+		
+		List<Etapa> etapa;
+
+		public Beneficio() {
+			
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public Long getId() {
+			return id;
+		}	
+		
+		
+		
+}
