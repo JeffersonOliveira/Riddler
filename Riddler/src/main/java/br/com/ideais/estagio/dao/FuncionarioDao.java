a8f01bc7f4cf7ebffb23753c14e5efbc01283e5f
Philippe <philippe@philippe-HP-Pavilion-g4-Notebook-PC.(none)>
2013-03-06 16:10:07 -0300

    Criando a classe FuncionarioDao

--- Riddler/src/main/java/br/com/ideais/estagio/dao/FuncionarioDao.java
+++ Riddler/src/main/java/br/com/ideais/estagio/dao/FuncionarioDao.java
@@ -3,48 +3,47 @@ package br.com.ideais.estagio.dao;
 import java.util.List;
 
 import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ideais.estagio.model.Funcionario;
 
-public class FuncionarioDao implements AbstractDao<Funcionario>{
+@Transactional
+public class FuncionarioDao implements AbstractDao<Funcionario> {
 
 	private HibernateTemplate hibernateTemplate;
-	
+
 	public FuncionarioDao(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
 	}
 
-	
 	public void persist(Funcionario funcionario) {
-		hibernateTemplate.persist(funcionario);		
+		hibernateTemplate.persist(funcionario);
 	}
 
-	public List<Funcionario> list() {		
+	public List<Funcionario> list() {
 		return hibernateTemplate.loadAll(Funcionario.class);
 	}
 
-	public Funcionario findById(Long id) {		
-		return hibernateTemplate.get(Funcionario.class,id);
+	public Funcionario findById(Long id) {
+		return hibernateTemplate.get(Funcionario.class, id);
 	}
 
 	public boolean delete(Funcionario id) {
-		try{
+		try {
 			hibernateTemplate.delete(id);
 			return true;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
 
 	public boolean saveOrUpdate(Funcionario funcionario) {
-		try{
+		try {
 			hibernateTemplate.saveOrUpdate(funcionario);
 			return true;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
 
-	
-	
 }
