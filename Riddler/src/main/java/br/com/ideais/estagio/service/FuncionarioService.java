5a0f531d24a95e0eb2dcbfd86fac5c3e678f1101
larissateixeira <larissa.teixeira@ideais.com.br>
2013-03-06 15:00:50 -0300

    Classe FuncionarioService

---
+++ Riddler/src/main/java/br/com/ideais/estagio/service/FuncionarioService.java
@@ -0,0 +1,32 @@
+package br.com.ideais.estagio.service;
+import br.com.ideais.estagio.dao.FuncionarioDao;
+import br.com.ideais.estagio.model.Funcionario;
+
+import java.util.List;
+
+public class FuncionarioService {
+
+	private FuncionarioDao fDao;
+	
+	public void persist(Funcionario funcionario){
+		fDao.persist(funcionario);
+	}
+	
+	public void saveOrUpdate(Funcionario funcionario){
+		fDao.saveOrUpdate(funcionario);
+	}
+
+	public List<Funcionario> list() {
+		return fDao.list();
+	}
+	
+
+	public Funcionario findbyId(Long id) {
+		return fDao.findById(id);
+	}
+	
+	public boolean delete(Funcionario funcionario){
+		return fDao.delete(funcionario);
+	}
+
+}
