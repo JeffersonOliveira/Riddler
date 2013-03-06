2c8c1aecef2eee181e06ad6daceff5d789137531
Philippe <philippe@philippe-HP-Pavilion-g4-Notebook-PC.(none)>
2013-03-06 16:30:39 -0300

    a interface CRUDAction foi criada

---
+++ Riddler/src/main/java/br/com/ideais/estagio/action/CRUDAction.java
@@ -0,0 +1,18 @@
+package br.com.ideais.estagio.action;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.Preparable;
+
+public interface CRUDAction extends Action,Preparable{
+
+	public String save();
+	
+	public String update();
+	
+	public String delete();
+	
+	public String create();
+	
+	public String list();
+	
+}
