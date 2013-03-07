ea0ed0dd1e0c24b90ac819235ef024af6efb0e2f
larissateixeira <larissa.teixeira@ideais.com.br>
2013-03-07 18:03:59 -0300

    moficacoes na classe FeitosAction

--- Riddler/src/main/java/br/com/ideais/estagio/action/FeitosAction.java
+++ Riddler/src/main/java/br/com/ideais/estagio/action/FeitosAction.java
@@ -1,59 +1,85 @@
-//package br.com.ideais.estagio.action;
-//
-//import java.util.List;
-//
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.apache.struts2.ServletActionContext;
-//
-//import com.opensymphony.xwork2.ActionContext;
-//
-//import br.com.ideais.estagio.model.Feitos;
-//import br.com.ideais.estagio.service.FeitosService;
-//
-//
-//public class FeitosAction implements CRUDAction{
-//
-//	
-//	private FeitosService feitosService;
-//	private Feitos feitos;
-//	private List<Feitos> feitos;
-//
-//	
-//	public String execute() throws Exception {
-//		return SUCESS;
-//	}
-//
-//	public void prepare() throws Exception {
-//		if(getFeitosFromRequest()!=null){
-//			feitos = feitosService.findById(getFeitosFromRequest());
-//	}
-//
-//	public String save() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public String update() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public String delete() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public String create() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public String list() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//	
-//
-//}
+package br.com.ideais.estagio.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+import br.com.ideais.estagio.model.Feitos;
+import br.com.ideais.estagio.service.FeitosService;
+
+import com.opensymphony.xwork2.ActionContext;
+
+public class FeitosAction implements CRUDAction {
+
+	private FeitosService feitosService;
+	private Feitos feitos;
+	private List<Feitos> list_feitos;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+	public void prepare() throws Exception {
+		if(getFeitosFromRequest()!=null){
+			feitos = feitosService.findById(getFeitosFromRequest());
+		}
+	}
+
+	private Long getFeitosFromRequest() {
+		HttpServletRequest request = (HttpServletRequest) ActionContext
+				.getContext().get(ServletActionContext.HTTP_REQUEST);
+		if (request.getParameter("id") != null)
+			return Long.parseLong(request.getParameter("id"));
+		return null;
+	}
+	
+	public FeitosService getFeitosService() {
+		return feitosService;
+	}
+	
+	public void setFeitosService(FeitosService feitosService) {
+		this.feitosService = feitosService;
+	}
+	
+	public Feitos getFeitos() {
+		return feitos;
+	}
+
+	public void setFeitos(Feitos feitos) {
+		this.feitos = feitos;
+	}
+
+	public List<Feitos> getListFeitos() {
+		return list_feitos;
+	}
+
+	public void setListFeitos(List<Feitos> list_feitos) {
+		this.list_feitos = list_feitos;
+	}
+	
+	public String save() {
+		feitosService.saveOrUpdate(getFeitos());
+		return SUCCESS;
+	}
+
+	public String update() {		
+		return SUCCESS;
+	}
+
+	public String delete() {
+		if(feitosService.delete(getFeitos()))
+			return SUCCESS;
+		return ERROR;
+	}
+
+	public String create() {		
+		return SUCCESS;
+	}
+
+	public String list() {
+		list_feitos = feitosService.list();
+		return SUCCESS;
+	}
+}
