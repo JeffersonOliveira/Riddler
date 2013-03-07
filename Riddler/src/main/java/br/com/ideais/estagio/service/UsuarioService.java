4ccefd643b0b3cba4f0b5d796f7150819903d6b1
Philippe <philippe@philippe-HP-Pavilion-g4-Notebook-PC.(none)>
2013-03-07 16:55:21 -0300

    adicionando a classe UsuarioService

---
+++ Riddler/src/main/java/br/com/ideais/estagio/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package br.com.ideais.estagio.service;
+
+import java.util.List;
+
+import br.com.ideais.estagio.dao.UsuarioDao;
+import br.com.ideais.estagio.model.Usuario;
+
+public class UsuarioService {
+
+	private UsuarioDao uDao;
+
+	public UsuarioService(UsuarioDao uDao) {
+
+		this.uDao = uDao;
+	}
+
+	public void persist(Usuario feitos) {
+
+		uDao.persist(feitos);
+
+	}
+
+	public void saveOrUpdate(Usuario usuario) {
+
+		uDao.saveOrUpdate(usuario);
+	}
+
+	public List<Usuario> list() {
+
+		return uDao.list();
+	}
+
+	public Usuario findById(Long id) {
+
+		return uDao.findById(id);
+	}
+
+	public boolean delete(Usuario usuario) {
+
+		return uDao.delete(usuario);
+	}
+}
