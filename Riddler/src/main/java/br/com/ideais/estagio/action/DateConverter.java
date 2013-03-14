0af518d92614021d28e302cf82c9a41a980e42a7
Douglas Mandarino <mandarino.douglas@me.com>
2013-03-13 22:00:35 -0300

    acerto de data no funcionario e concerto na classe FuncionarioAction

---
+++ Riddler/src/main/java/br/com/ideais/estagio/action/DateConverter.java
@@ -0,0 +1,29 @@
+package br.com.ideais.estagio.action;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import org.apache.struts2.util.StrutsTypeConverter;
+
+public class DateConverter extends StrutsTypeConverter{
+
+
+	public Object convertFromString(Map context, String[] value, Class toClass) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = null;
+		System.out.println("========================= ================\n chamei o converter!!!!!!");
+		try {
+			date = sdf.parse(value[0]);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+	public String convertToString(Map context, Object o) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		return sdf.format(o);
+	}
+
+}
