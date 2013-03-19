76ff1e3f5fcba04137162d93faef5ac36d125ab5
larissateixeira <larissa.teixeira@ideais.com.br>
2013-03-19 13:57:47 -0300

    Organizacao dos metodos

--- Riddler/src/main/java/br/com/ideais/estagio/action/BeneficioAction.java
+++ Riddler/src/main/java/br/com/ideais/estagio/action/BeneficioAction.java
@@ -7,56 +7,80 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.ideais.estagio.model.Beneficio;
+import br.com.ideais.estagio.model.Etapa;
+import br.com.ideais.estagio.service.BeneficioService;
+import br.com.ideais.estagio.service.EtapaService;
+
 import com.opensymphony.xwork2.ActionContext;
 
-import br.com.ideais.estagio.model.Beneficio;
-import br.com.ideais.estagio.service.BeneficioService;
-
-public class BeneficioAction implements CRUDAction{
+public class BeneficioAction implements CRUDAction {
 
 	@Autowired
 	private BeneficioService beneficioService;
+	@Autowired
+	private EtapaService etapaService;
 	private Beneficio beneficio;
 	private List<Beneficio> beneficios;
-	
-	
-	public void setBeneficio(Beneficio beneficio){
-		this.beneficio = beneficio;
-	}
-	
+
 	public String execute() throws Exception {
 		return SUCCESS;
 	}
 
 	public void prepare() throws Exception {
-		if(getBeneficioFromRequest()!=null){
+		if (getBeneficioFromRequest() != null) {
 			beneficio = beneficioService.findbyId(getBeneficioFromRequest());
 		}
-		
 	}
-	
-	private Long getBeneficioFromRequest() {
-		HttpServletRequest request = (HttpServletRequest)ActionContext.getContext()
-				   .get(ServletActionContext.HTTP_REQUEST);
-		
-		if(request.getParameter("id")!=null)
-			return Long.parseLong(request.getParameter("id"));
 
+	public String create() {
+		return SUCCESS;
+	}
+
+	public String save() {
+		if (beneficio.getNome().equals("")) {
+			erroCampoVazio();
+			return ERROR;
+		}
+		if (beneficioService.saveOrUpdate(beneficio)) {
+			return SUCCESS;
+		} else {
+			erroAoCriarBeneficio();
+			return ERROR;
+		}
+	}
+
+	public String update() {
+		return SUCCESS;
+	}
+
+	public String delete() {
+		if (beneficioService.delete(getBeneficio()))
+			return SUCCESS;
+		return ERROR;
+	}
+
+	public String list() {
+		beneficios = beneficioService.list();
+		return SUCCESS;
+	}
+
+	private Long getBeneficioFromRequest() {
+		HttpServletRequest request = (HttpServletRequest) ActionContext
+				.getContext().get(ServletActionContext.HTTP_REQUEST);
+
+		if (request.getParameter("id") != null){
+			return Long.parseLong(request.getParameter("id"));
+		}
 		return null;
 	}
-	
-	
-	// REDIRECIONA A MESSAGEM DE ERRO PARA A TELA
-	 
-	private void erroAoCriarBeneficio() {
-		HttpServletRequest request = (HttpServletRequest)ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-		request.setAttribute("erro", "Beneficio Existente");
+
+	public void setBeneficio(Beneficio beneficio) {
+		this.beneficio = beneficio;
 	}
-	
-	//
-	
-	public BeneficioService getBeneficioService() {
-		return beneficioService;
+
+	public void setBeneficios(List<Beneficio> beneficios) {
+		this.beneficios = beneficios;
 	}
 
 	public void setBeneficioService(BeneficioService beneficioService) {
@@ -71,45 +95,20 @@ public class BeneficioAction implements CRUDAction{
 		return beneficios;
 	}
 
-	public void setBeneficios(List<Beneficio> beneficios) {
-		this.beneficios = beneficios;
+	public BeneficioService getBeneficioService() {
+		return beneficioService;
 	}
 
-	public String save() {
-		if (beneficio.getNome().equals("")){
-			erroCampoVazio();
-			return ERROR;
-		}
-		if(beneficioService.saveOrUpdate(beneficio)){
-			return SUCCESS;
-		}
-		else{
-			erroAoCriarBeneficio();
-			return ERROR;
-		}
+	private void erroAoCriarBeneficio() {
+		HttpServletRequest request = (HttpServletRequest) ActionContext
+				.getContext().get(ServletActionContext.HTTP_REQUEST);
+		request.setAttribute("erro", "Beneficio Existente");
 	}
+
 	private void erroCampoVazio() {
-		HttpServletRequest request = (HttpServletRequest)ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-		request.setAttribute("erro", "Preencha os campos, por favor");		
-	}
-
-	public String update() {		
-		return SUCCESS;
-	}
-
-	public String delete() {
-		if(beneficioService.delete(getBeneficio()))
-			return SUCCESS;
-		return ERROR;
-	}
-
-	public String create() {		
-		return SUCCESS;
-	}
-
-	public String list() {
-		beneficios = beneficioService.list();
-		return SUCCESS;
+		HttpServletRequest request = (HttpServletRequest) ActionContext
+				.getContext().get(ServletActionContext.HTTP_REQUEST);
+		request.setAttribute("erro", "Preencha os campos, por favor");
 	}
 
 }
