4b609ad6be8051ed03febe7a621da822af2b204f
Philippe <philippe@philippe-HP-Pavilion-g4-Notebook-PC.(none)>
2013-03-20 17:56:07 -0300

    adicionando o atributo dataAlarme

--- Riddler/src/main/java/br/com/ideais/estagio/model/Alarme.java
+++ Riddler/src/main/java/br/com/ideais/estagio/model/Alarme.java
@@ -1,24 +1,33 @@
 package br.com.ideais.estagio.model;
 
+import java.util.Date;
+
 public class Alarme {
 
 	private Long codigo;
+	private Date dataAlarme;
 	private Funcionario funcionario;
 	private Beneficio beneficio;
 	
 	public Alarme() {
 		
 	}
-	
-	public Alarme(Long codigo, Funcionario funcionario, Beneficio beneficio) {
+
+	public Alarme(Long codigo, Date dataAlarme, Funcionario funcionario,
+			Beneficio beneficio) {
 		this.codigo = codigo;
+		this.dataAlarme = dataAlarme;
 		this.funcionario = funcionario;
 		this.beneficio = beneficio;
 	}
 
-
+	
 	public Long getCodigo() {
 		return codigo;
+	}
+	
+	public Date getDataAlarme() {
+		return dataAlarme;
 	}
 	
 	public Funcionario getFuncionario() {
